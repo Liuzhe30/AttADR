8a442c5c4716d771dc35b38711a84eda63b0a691
刘喆 <32893370+Liuzhe30@users.noreply.github.com>
2021-04-05 23:01:47 +0800

    multi-head attention

--- simulation/model.py
+++ simulation/model.py
@@ -33,98 +33,6 @@ c = 202
 d = 957
 e = 500
 
-class MultiHeadSelfAttention(layers.Layer):
-    def __init__(self, embed_dim, num_heads=8):
-        super(MultiHeadSelfAttention, self).__init__()
-        self.embed_dim = embed_dim
-        self.num_heads = num_heads
-        if embed_dim % num_heads != 0:
-            raise ValueError(
-                f"embedding dimension = {embed_dim} should be divisible by number of heads = {num_heads}"
-            )
-        self.projection_dim = embed_dim // num_heads
-        self.query_dense = layers.Dense(embed_dim)
-        self.key_dense = layers.Dense(embed_dim)
-        self.value_dense = layers.Dense(embed_dim)
-        self.combine_heads = layers.Dense(embed_dim)
-
-    def attention(self, query, key, value, mask):
-        score = tf.matmul(query, key, transpose_b=True)
-        dim_key = tf.cast(tf.shape(key)[-1], tf.float32)
-        scaled_score = score / tf.math.sqrt(dim_key)
-        if mask is not None:
-            scaled_score += (mask * -1e9)
-        weights = tf.nn.softmax(scaled_score, axis=-1)
-        output = tf.matmul(weights, value)
-        return output, weights
-
-    def separate_heads(self, x, batch_size):
-        x = tf.reshape(x, (batch_size, -1, self.num_heads, self.projection_dim))
-        return tf.transpose(x, perm=[0, 2, 1, 3])
-
-    def call(self, inputs, mask=None):
-        # x.shape = [batch_size, seq_len, embedding_dim]
-        batch_size = tf.shape(inputs)[0]
-        query = self.query_dense(inputs)  # (batch_size, seq_len, embed_dim)
-        key = self.key_dense(inputs)  # (batch_size, seq_len, embed_dim)
-        value = self.value_dense(inputs)  # (batch_size, seq_len, embed_dim)
-        query = self.separate_heads(
-            query, batch_size
-        )  # (batch_size, num_heads, seq_len, projection_dim)
-        key = self.separate_heads(
-            key, batch_size
-        )  # (batch_size, num_heads, seq_len, projection_dim)
-        value = self.separate_heads(
-            value, batch_size
-        )  # (batch_size, num_heads, seq_len, projection_dim)
-        attention, weights = self.attention(query, key, value, mask)
-        attention = tf.transpose(
-            attention, perm=[0, 2, 1, 3]
-        )  # (batch_size, seq_len, num_heads, projection_dim)
-        concat_attention = tf.reshape(
-            attention, (batch_size, -1, self.embed_dim)
-        )  # (batch_size, seq_len, embed_dim)
-        output = self.combine_heads(
-            concat_attention
-        )  # (batch_size, seq_len, embed_dim)
-        return output
-    
-    def get_config(self):
-        config = super().get_config().copy()
-        config.update({
-            'embed_dim': self.embed_dim,
-        })
-        return config
-    
-class TransformerBlock(layers.Layer):
-    def __init__(self, embed_dim, num_heads, ff_dim, rate=0.1):
-        super(TransformerBlock, self).__init__()
-        self.att = MultiHeadSelfAttention(embed_dim, num_heads)
-        self.ffn = keras.Sequential(
-            [layers.Dense(ff_dim, activation="relu"), layers.Dense(embed_dim),]
-        )
-        self.layernorm1 = layers.LayerNormalization(epsilon=1e-6)
-        self.layernorm2 = layers.LayerNormalization(epsilon=1e-6)
-        self.dropout1 = layers.Dropout(rate)
-        self.dropout2 = layers.Dropout(rate)
-
-    def call(self, inputs, mask=None, training=True):
-        attn_output = self.att(inputs, mask)
-        attn_output = self.dropout1(attn_output, training=training)
-        out1 = self.layernorm1(inputs + attn_output)
-        ffn_output = self.ffn(out1)
-        ffn_output = self.dropout2(ffn_output, training=training)
-        return self.layernorm2(out1 + ffn_output)
-    
-    def get_config(self):
-        config = super().get_config().copy()
-        config.update({
-              'embed_dim': embed_dim,
-              'num_heads': num_heads,
-              'ff_dim': ff_dim,
-        })
-        return config
-
 def xdata_generator():
     drug_profile = np.loadtxt('data/drug_profile.npy') # N * (a + b + c + d + e)
     #similarity_mat = np.loadtxt('data/similarity_mat.npy') # 5N * N
@@ -194,7 +102,7 @@ if __name__ == "__main__":
     print('AE1.get_shape()', AE1.get_shape())
     
     profile_merged = tf.stack([AE1, AE2, AE3, AE4, AE5], axis=1)
-    print('profile_merged.get_shape()', profile_merged.get_shape())
+    print('profile_merged.get_shape()', profile_merged.get_shape()) # 5 * N  ,(?, 5, 100)
     
     ## drug similarity
     input6 = Input(shape=(N, N),name = 'drug_similarity_a')
@@ -204,11 +112,19 @@ if __name__ == "__main__":
     input10 = Input(shape=(N, N),name = 'drug_similarity_e')
     
     similarity_merged = concatenate([input6, input7, input8, input9, input10], axis=1)
-    print('similarity_merged.get_shape()', similarity_merged.get_shape())
+    print('similarity_merged.get_shape()', similarity_merged.get_shape())  # 5N * N , (?, 500, 100)
     
-    final_merged = concatenate([profile_merged, similarity_merged], axis=1)
-    print('final_merged.get_shape()', final_merged.get_shape())
+    final_merged = concatenate([profile_merged, similarity_merged], axis=1) 
+    print('final_merged.get_shape()', final_merged.get_shape())  # 5(N + 1) * N  , (?, 505, 100)
 
     final_merged_trans = tf.transpose(final_merged, perm = (0, 2, 1))
-    print('final_merged_trans.get_shape()', final_merged_trans.get_shape())
+    print('final_merged_trans.get_shape()', final_merged_trans.get_shape())  # N * 5(N + 1) , (?, 100, 505)
+    
+    # MultiHeadAttention
+    from keras_multi_head import MultiHeadAttention  # pip install keras_multi_head
+    att1 = MultiHeadAttention(head_num = N, name = 'Multi-Head',)(final_merged)
+    print('att1.get_shape()', att1.get_shape())
+    att2 = MultiHeadAttention(head_num= 5 * (N + 1), name = 'Multi-Head',)(final_merged_trans)
+    print('att2.get_shape()', att2.get_shape())
+    
     
