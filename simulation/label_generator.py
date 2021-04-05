3dc365b222d919eaf1d4a8c90f3e86bf74e6aa76
刘喆 <32893370+Liuzhe30@users.noreply.github.com>
2021-04-05 21:26:19 +0800

    generate label

--- simulation/label_generator.py
+++ simulation/label_generator.py
@@ -11,13 +11,13 @@ d = 957
 e = 500
 
 label_list = []
-for i in range(0, edge + 1):
-    pair = np.random.randint(edge, high = None, size = 2, dtype = 'l')
+for i in range(0, edge + 20):
+    pair = np.random.randint(N, high = None, size = 2, dtype = 'l')
     pair.sort()
     label_list.append(pair)
 
 corr = np.array(list(set([tuple(t) for t in label_list])))
-
+print(corr.shape)
 cla = np.random.randint(4, high = None, size = (edge, 1), dtype = 'l')
 
 label_mat = np.c_[corr, cla]
